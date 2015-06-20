0c7a692f56bfde1585f08d70dcd6c464122c6475
ChasonPickles <harshbindra@live.ca>
2015-06-20 11:55:02 -0400

    add horizontal bars

--- app/src/main/java/com/example/android/modernart/MainActivity.java
+++ app/src/main/java/com/example/android/modernart/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.modernart;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View rect1 = findViewById(R.id.rect1);
+        rect1.setBackgroundColor(0xFF00FF00);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
     }
 
     @Override
