a6a894d470b91529d85284563dad2f630ed2c2bb
gautam14040 <gautam14040@iiitd.ac.in>
2018-03-22 01:37:06 +0530

    Silder Added

--- app/src/main/java/com/example/android/modernart/MainActivity.java
+++ app/src/main/java/com/example/android/modernart/MainActivity.java
@@ -23,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        SeekBar seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
 
         //Display display = getWindowManager().getDefaultDisplay();
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
 
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+//                SeekBar seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
+//                seekBar2.setProgress(progressValue);
 
                 View red_rect1 = findViewById(R.id.first);
                 View red_rect2 = findViewById(R.id.third);
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 int red_rects_scale = 0xFFFF0000 + (progressValue*(0x00001100));
                 int white_rects_scale = 0xFFFFFFFF - (progressValue*(0x00111111));
 
-                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
                 if (red_rect1 != null && red_rect2 != null && red_rect3 != null) {
                     red_rect1.setBackgroundColor(red_rects_scale);
                     red_rect2.setBackgroundColor(red_rects_scale);
@@ -66,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onStartTrackingTouch(SeekBar seekBar) {
 
-                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
 
             }
 
@@ -74,10 +77,65 @@ public class MainActivity extends AppCompatActivity {
 
             public void onStopTrackingTouch(SeekBar seekBar) {
                 updateRectangles();
-                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                SeekBar seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
+                seekBar2.setProgress(progressValue);
+
+                View red_rect1 = findViewById(R.id.first);
+                View red_rect2 = findViewById(R.id.third);
+                View red_rect3 = findViewById(R.id.fifth);
+                View red_rect4 = findViewById(R.id.horizontal_rect1);
+                View red_rect5 = findViewById(R.id.horizontal_rect2);
+
+                View white_rect1 = findViewById(R.id.second);
+                View white_rect2 = findViewById(R.id.fourth);
+
+
+
+                int red_rects_scale = 0xFFFF0000 + (progressValue*(0x00001100));
+                int white_rects_scale = 0xFFFFFFFF - (progressValue*(0x00111111));
+
+//                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+                if (red_rect1 != null && red_rect2 != null && red_rect3 != null) {
+                    red_rect1.setBackgroundColor(red_rects_scale);
+                    red_rect2.setBackgroundColor(red_rects_scale);
+                    red_rect3.setBackgroundColor(red_rects_scale);
+                    red_rect4.setBackgroundColor(red_rects_scale);
+                    red_rect5.setBackgroundColor(red_rects_scale);
+                }
+
+                if (white_rect1 != null && white_rect2 != null) {
+                    white_rect1.setBackgroundColor(white_rects_scale);
+                    white_rect2.setBackgroundColor(white_rects_scale);
+                }
+
+            }
+
+            @Override
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+//                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                updateRectangles();
+//                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
 
 
     public void updateRectangles(){
